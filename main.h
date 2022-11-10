89db709567b38cd3cf5ca2666f8f5654d16d2e87
khumilla <hold.the.dianhua@gmail.com>
2022-11-10 19:10:08 +0000

    main files incomplete files

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int ac, char **argv);
+int isatty(int fd);
+
+
+
+
+#endif /* MAIN_H */
---
+++ shell.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <unistd.h>
+
+/** main - function to see if file descriptor refers to terminal
+ * return: 1 on success; err otherwise.
+ */
+
+
+
+
