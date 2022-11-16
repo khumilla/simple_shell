8a4b719bb5029d889bd2da8e10b6e1a195a2830b
khumilla <hold.the.dianhua@gmail.com>
2022-11-16 10:45:12 +0000

    updated make authors

--- make_authors.sh
+++ make_authors.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
  set -e
 
- cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
-
-# see also ".mailmap" for how email addresses and names are deduplicated
+ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 {
 	cat <<- 'EOH'
 		# This file lists all individuals having contributed content to the repository.
-		# For how it is generated, see `hack/generate-authors.sh`.
+		# For how it is generated, see `https://github.com/moby/moby/blob/master/hack/generate-authors.sh`.
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
