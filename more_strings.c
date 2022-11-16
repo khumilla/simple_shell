a12c71da73e91cd799f4ae2f372e4b9d7677960e
Shaix33 <KevinNdlondlo@gmail.com>
2022-11-16 06:13:30 -0800

    String tokenizer

---
+++ more_strings.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: given string
+ *
+ * Return: pointer to duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	int size, x;
+	char *dest;
+
+	if (!str)
+		return (NULL);
+
+	size = _strlen(str) + 1;
+	dest = malloc(size * sizeof(char));
+
+	if (!dest)
+		return (NULL);
+
+	for (x = 0; x < size; x++)
+
+		*(dest + x) = *(str + x);
+
+		*(dest + x) = '\0';
+
+		return (dest);
+}
